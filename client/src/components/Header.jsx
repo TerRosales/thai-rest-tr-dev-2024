142e7c1be6aa28a2ea40a9a80e686d7984566688
RTIN <118107221+Romo3@users.noreply.github.com>
2024-05-22 13:39:58 -0400

    adding framer, header, hero layouts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,33 +4,31 @@ import "./styles/header.css";
 import images from "../assets/images.js";
 const Header = () => {
   return (
-    <Navbar fluid rounded className="w-full h-14">
-      <Navbar.Brand href="/" className="flex flex-col">
+    // addintional styles for navbar at header.css
+    <Navbar fluid className="border-red-500 border-[1px] border-t-[0]shadow-lg shadow-red-500/60 rounded-2xl w-[60%] mx-auto">
+      <Navbar.Brand href="/" className="mb-2">
         <img
           src={images.utility.thaiBrand}
           alt="thai rest logo"
-          className="w-24 h-24"
+          className="absolute w-20 h-20"
         />
-        {/* <span className="absolute font-extrabold text-[14px] text-orange-700 mt-16 bg-gradient-to-tl from-yellow-200 to-purple-400 p-1 rounded-xl border-2 border-purple-900">
+        <span className="absolute font-extrabold text-[14px] text-orange-700 mt-16 bg-gradient-to-tl from-yellow-200 to-purple-400 p-1 rounded-xl border-2 border-purple-900">
           Thai-Rest
-        </span> */}
+        </span>
       </Navbar.Brand>
       {/* <Button className="" gradientDuoTone="purpleToPink" outline>
         Combo Builder
       </Button> */}
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Navbar.Link>
+      <Navbar.Toggle className="h-10 text-red-600"/>
+      <Navbar.Collapse className="h-10
+      pt-2">
         <Link to="/">
           Home
         </Link>
-        </Navbar.Link>
-        <Navbar.Link>
+        
         <Link to="/about">About</Link>
-        </Navbar.Link>
-        <Navbar.Link>
+        
         <Link to="/menu">Menu</Link>
-        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,20 +1,32 @@
+import { useState } from "react";
+import { motion, spring } from "framer-motion";
 import Header from "./Header";
 import "./styles/hero.css";
+import images from "../assets/images.js";
 
 const HeroSection = () => {
   return (
     <section className="heroSection">
-      <h1 className="heroTitle">Thai Rest 24</h1>
-      <p className="heroIntro">
+      <div
+        
+        >
+        <motion.img 
+          initial={{ x: -100, opacity: 0.9, rotate: -15 }}
+          animate={{x: 0, opacity: 1, rotate: [45,30,5], x: 0}} src={images.utility.thaiBrand} 
+          transition={{ duration: 2,  type: "spring", stiffness: 300  }}
+          alt="thai rest logo" className="absolute h-[10rem] w-[10rem]"/>
+      </div>
+      <div className="heroIntro pt-[14%] pb-[7%] sm:pt-[6%]">
+      <h1 className="heroTitle">Thai Rest<span className="text-white text-6xl">.&nbsp;</span>24</h1>
+      <p className="heroText mt-5">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
         doloribus debitis dolore nostrum maiores error, quam illum minima veniam
         accusantium laudantium iste quis delectus nemo quibusdam ad eligendi
         maxime necessitatibus. Quod nam dignissimos eligendi repudiandae! Ad,
         ipsa quibusdam. Illo, nisi.
       </p>
-      <div className="headerContainer">
-        <Header />
       </div>
+        <Header />
       <div className="scrollToButtom"></div>
     </section>
   );
